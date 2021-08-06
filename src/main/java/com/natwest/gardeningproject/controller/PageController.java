e7bb3daff24581d0b75a31f0fa5e4f2c5b4a17e3
Anton.Ellis <Anton.Ellis2@outlook.com>
2021-08-06 16:01:09 +0100

    Page controller updated

--- src/main/java/com/natwest/gardeningproject/controller/PageController.java
+++ src/main/java/com/natwest/gardeningproject/controller/PageController.java
@@ -1,9 +1,18 @@
 package com.natwest.gardeningproject.controller;
 
+
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class PageController {
+	
+	@GetMapping("/home")
+	public String home() {
+		return "index";
+			}
 	
 	
 
