2c054489a6dc256d49d3c94e0091fff800041213
Anton.Ellis <Anton.Ellis2@outlook.com>
2021-08-06 12:46:37 +0100

    Plant Controller Update

--- src/main/java/com/natwest/gardeningproject/controller/PlantController.java
+++ src/main/java/com/natwest/gardeningproject/controller/PlantController.java
@@ -1,5 +1,56 @@
 package com.natwest.gardeningproject.controller;
 
-public class PlantController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.natwest.gardeningproject.persistence.domain.Plant;
+import com.natwest.gardeningproject.service.PlantService;
+
+@RestController
+public class PlantController {
+	
+	public PlantController() {
+		
+	}
+	
+	@Autowired
+	private PlantService service;
+	
+	@GetMapping("/read")
+	public ResponseEntity<List<Plant>> getAll(){
+		return new ResponseEntity<List<Plant>>(this.service.readAll(), HttpStatus.OK);
+	}
+	
+	@PostMapping("/create")
+	public ResponseEntity<Plant> createPlant(@RequestBody Plant plant){
+		return new ResponseEntity<Plant>(this.service.createFruit(plant), HttpStatus.CREATED);
+	}
+	
+	@GetMapping("/findbyid/{id}")
+	public ResponseEntity<Plant> findById (@PathVariable ("id") Long id){
+		return new ResponseEntity<Plant>(this.service.findById(id), HttpStatus.FOUND);
+	}
+		
+	@PutMapping("update/{id}")
+	public ResponseEntity<Plant> update (@PathVariable("id") Long id, @RequestBody Plant plant){
+		return new ResponseEntity<Plant>(this.service.updateFruit(id, plant), HttpStatus.ACCEPTED);
+	}
+		
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<Boolean> delete (@PathVariable Long id){
+		return new ResponseEntity<Boolean>(this.service.deletePlant(id), HttpStatus.NO_CONTENT);
+	}
+	
+	
 }
+
