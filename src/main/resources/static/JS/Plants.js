210a90a669e767ebecf5ff5e6a569bd45a175d25
Anton.Ellis <Anton.Ellis2@outlook.com>
2021-08-09 15:01:48 +0100

    .js update

--- src/main/resources/static/JS/Plants.js
+++ src/main/resources/static/JS/Plants.js
@@ -1,20 +1,11 @@
-const PtTable = `<div class="plant">
-				<table style="width:80%">
-			    <tr>
-				<th>ID</th>
-				<th>Name</th>
-				<th>Price</th>
-                <th>Size</th>
-                <th>Variegated</th>
-				<tr>`;
-const PbTable = `</table>
-				</div>`;
 
-function showDB() {
-    const body = document.body;
-    const para = document.createElement('p');
-    body.append(para);
-    fetch("http://localhost:9002/read")
-.then(response => response.json())
-.then(data => console.log(data))
-}
+
+fetch("http://localhost:9002/read")
+    .then((response) => {
+        if (response.status !== 200) {
+            console.error(`status: ${reponse.status}`);
+            return;
+        }
+        response.json()
+        .then(data => console.info(data))
+    }).catch((err)=> console.error(`${err}`));
